427730d70abb9f50f0ac651d1aca66465fedbebd
myusernameismy <ilyas123.822@gmail.com>
2025-04-08 17:46:31 +0500

    wasd try

--- main.py
+++ main.py
@@ -3,39 +3,52 @@ import os
 import sys
 import termios
 import tty
+import time
+import select
 from minicubebase import *
 
-fd = sys.stdin.fileno()
-old_settings = termios.tcgetattr(fd)
+def get_pressed_key(timeout=0.01):
+    dr, dw, de = select.select([sys.stdin], [], [], timeout)
+    if dr:
+        return sys.stdin.read(1)
+    return None
 
-def getch():
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+def set_terminal_raw():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    tty.setcbreak(fd)
+    return old_settings
+
+def restore_terminal_settings(old_settings):
+    termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, old_settings)
 
 DEVICE_NAME = '/dev/ttyUSB0'
-
 motor1 = MotorV1(DEVICE_NAME, 1)
 motor1.enable_torque()
 motor1.set_mode('WHEEL_MODE')
 
+print("Hold [w] to move forward, [s] to move backward, [q] to quit.")
+
+old_settings = set_terminal_raw()
+
 try:
     while True:
-        ch = getch()
-        if ch == 'w':
+        key = get_pressed_key()
+        if key == 'w':
             motor1.move_forward()
-        elif ch == 's':
+        elif key == 's':
             motor1.move_backward()
-        elif ch == 'q':
-            print("Exiting...")
+        elif key == 'q':
+            print("Quitting...")
             break
+        elif key is not None:
+            motor1.stop_move()
         else:
             motor1.stop_move()
-        time.sleep(0.1)
+        time.sleep(0.01)
+
 finally:
+    restore_terminal_settings(old_settings)
     motor1.stop_move()
     motor1.disable_torque()
     motor1.portHandler.closePort()
-
